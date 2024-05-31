f7e73d85633fb5c1cb672dbc90dee78d2f74319a
SUHANIS29 <suhanisuryawanshi2935@gmail.com>
2024-05-31 14:20:25 +0530

    Create basic.c

---
+++ basic.c
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      body{
+  font-family: 'Segoe UI', Tahoma;
+  text-align: center;
+  letter-spacing:2px;
+  background-color: rgb(249, 127, 233);
+  padding: 20px;
+}
+h2,h4{
+  font-size: 2em;
+  width:100%;
+  margin-top: 10px;
+  }
+  .container{
+    display:flex;
+    justify-content:center;
+    flex-wrap:wrap;
+  }
+    </style>
+  </head>
+  <body>
+    <header>
+        <h1>
+          INDIA
+        </h1>
+        <h4>
+          Let's Explore our INDIA <br>
+        </h4>
+        <h2>
+          Welcome
+        </h2>
+        <p>
+          vision
+        </p>
+        <p>
+          About Us
+        </p>
+        <section class="container">
+          <article class="card">
+            <div class="icon">&#x1F60E; </div>
+          </article>
+        </section>
+    </header>
+  </body>
+</html>
