231cdb76dc5912348befef1152c6cec580b5691a
SUHANIS29 <suhanisuryawanshi2935@gmail.com>
2024-07-05 23:20:03 +0530

    Create ecom.js

---
+++ ecom.js
@@ -0,0 +1,12 @@
+//swipe
+var swiper = new Swiper(".home", {
+
+  spaceBetween: 30,
+centeredSlides:true,
+
+  
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
